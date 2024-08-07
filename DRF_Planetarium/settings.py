b2b0987e178c863f853922f9559755c0dd0bcaec
Sergii Musiienko <vpnmus@ukr.net>
2024-08-07 19:30:00 +0300

    setup jws tokens

--- DRF_Planetarium/settings.py
+++ DRF_Planetarium/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "debug_toolbar",
     "rest_framework",
+    "rest_framework.authtoken",
     "planetarium",
     "user",
 ]
@@ -131,3 +133,29 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "user.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ),
+}
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
+    "VERIFYING_KEY": None,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+}
+# REST_FRAMEWORK = {
+#     "DEFAULT_AUTHENTICATION_CLASSES": (
+#         "rest_framework.authentication.TokenAuthentication",
+#     ),
+#     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+# }
--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,50 @@
-from django.urls import path
 from user.views import CreateUserView, ManageUserView, CreateTokenView
+from rest_framework_simplejwt import views as jwt_views
+from django.http import JsonResponse
+from django.urls import path
+from django.urls import reverse
+
+
+def api_root(request):
+    return JsonResponse(
+        {
+            "register": request.build_absolute_uri(reverse("user:create")),
+            "token": request.build_absolute_uri(reverse("user:token")),
+            "me": request.build_absolute_uri(reverse("user:manage")),
+            "jwt_token_obtain": request.build_absolute_uri(
+                reverse("user:jwt_token_obtain_pair")
+            ),
+            "jwt_token_refresh": request.build_absolute_uri(
+                reverse("user:jwt_token_refresh")
+            ),
+            "jwt_token_verify": request.build_absolute_uri(
+                reverse("user:jwt_token_verify")
+            ),
+        }
+    )
+
 
 app_name = "user"
 
+
 urlpatterns = [
+    path("", api_root, name="api_root"),
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", CreateTokenView.as_view(), name="login"),
+    path("token/", CreateTokenView.as_view(), name="token"),
     path("me/", ManageUserView.as_view(), name="manage"),
+    path(
+        "jws_token/",
+        jwt_views.TokenObtainPairView.as_view(),
+        name="jws_token_obtain_pair",
+    ),
+    path(
+        "jws_token/refresh/",
+        jwt_views.TokenRefreshView.as_view(),
+        name="jws_token_refresh",
+    ),
+    path(
+        "jws_token/verify/",
+        jwt_views.TokenVerifyView.as_view(),
+        name="jws_token_verify",
+    ),
 ]
