b5f62d0985d12e3a5f1c8c0cfd008594ab8ac008
Sergii Musiienko <vpnmus@ukr.net>
2024-08-07 11:58:34 +0300

    create tables for app planetarium

--- planetarium/models.py
+++ planetarium/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from DRF_Planetarium import settings
+
+
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    show_theme = models.ManyToManyField(ShowTheme)
+
+    def __str__(self):
+        return self.title
+
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=100)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}(rows: {self.rows},seats: {self.seats_in_row})"
+
+
+class ShowSession(models.Model):
+    astronomy_show = models.ForeignKey(
+        AstronomyShow, on_delete=models.CASCADE, related_name="ShowSessions"
+    )
+    planetarium_dome = models.ForeignKey(
+        PlanetariumDome, on_delete=models.CASCADE, related_name="ShowSessions"
+    )
+    show_time = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ["show_time"]
+
+    def __str__(self):
+        return f" {self.astronomy_show.title} at {self.show_time}"
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"reservation at {self.created_at.strftime("%Y-%m-%d %H:%M:%S")}"
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    show_session = models.ForeignKey(
+        ShowSession, on_delete=models.CASCADE, related_name="tickets"
+    )
+    reservation = models.ForeignKey(
+        Reservation, on_delete=models.CASCADE, related_name="tickets"
+    )
+
+    def __str__(self):
+        return f"{self.show_session.astronomy_show.title}" f"({self.row}:{self.seat})"
