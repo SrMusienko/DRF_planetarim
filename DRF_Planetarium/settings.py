f8038882ad2366756d9aa4eceefaa1f1116471f7
Sergii Musiienko <vpnmus@ukr.net>
2024-08-09 09:11:42 +0300

    add general permission throw my own class

--- DRF_Planetarium/settings.py
+++ DRF_Planetarium/settings.py
@@ -141,6 +141,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ),
+    "DEFAULT_PERMISSION_CLASSES": ("planetarium.permissions.IsAdminOrIfAuthenticatedReadOnly",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
