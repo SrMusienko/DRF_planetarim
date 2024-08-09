2e15766528b5af1d74d38b8cb39ab00e5274d4cd
Sergii Musiienko <vpnmus@ukr.net>
2024-08-09 11:45:57 +0300

    del astronomy_show from theme

--- planetarium/serializers.py
+++ planetarium/serializers.py
@@ -8,7 +8,7 @@ from planetarium.models import ShowTheme, AstronomyShow, PlanetariumDome, ShowSe
 class ShowThemeSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShowTheme
-        fields = ("id", "name", "astronomy_shows")
+        fields = ("id", "name")
 
 
 class AstronomyShowSerializer(serializers.ModelSerializer):
