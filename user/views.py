087a49f6c0b488801b2009585902d46c0a7f4868
Sergii Musiienko <vpnmus@ukr.net>
2024-08-09 10:35:19 +0300

    change permission for normal register

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import AllowAny
 from rest_framework.settings import api_settings
 
 from user.serializers import UserSerializer, AuthTokenSerializer
@@ -7,11 +8,15 @@ from user.serializers import UserSerializer, AuthTokenSerializer
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    authentication_classes = []
+    permission_classes = [AllowAny]
 
 
 class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
     serializer_class = AuthTokenSerializer
+    authentication_classes = []
+    permission_classes = [AllowAny]
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
