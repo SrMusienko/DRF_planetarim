1d1eb37157148143a0db9a01fb4af4fe609aba2f
Sergii Musiienko <vpnmus@ukr.net>
2024-08-10 10:45:03 +0300

    add basename to urls for using in tests

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -11,9 +11,9 @@ from planetarium.views import (
 app_name = "planetarium"
 
 router = routers.DefaultRouter()
-router.register("show_themes", ShowThemeViewSet)
-router.register("astronomy_shows", AstronomyShowViewSet)
+router.register("show_themes", ShowThemeViewSet, )
+router.register("astronomy_shows", AstronomyShowViewSet, basename="astronomy_show")
 router.register("planetarium_domes", PlanetariumDomeViewSet)
-router.register("show_sessions", ShowSessionViewSet)
+router.register("show_sessions", ShowSessionViewSet, basename="show_session")
 router.register("reservations", ReservationViewSet)
 urlpatterns = [path("", include(router.urls))]
